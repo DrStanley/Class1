8261e5afbe972ce469d76b16ae446b67a7dbad78
Ozoezi stanley chukwujekwu <43678258+DrStanley@users.noreply.github.com>
2020-07-14 10:27:18 +0100

    Add project files.

---
+++ Class1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Class1.Services;
+
+namespace Class1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = UserInfo.GetName();
+            var age = UserInfo.GetAge();
+            Console.WriteLine("Name: {0}\nAge: {1}",name,age);
+            Console.WriteLine("My name is {0}, and i am {1} year old",name,age);
+
+        }
+    }
+}
---
+++ Class1/Services/UserInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class1.Services
+{
+	class UserInfo
+	{
+
+		public static string GetName() {
+			Console.WriteLine("Enter your name");
+			//reads users input from console
+			string getName = Console.ReadLine();
+			return getName;
+		}
+		public static int GetAge() {
+			int age = 0;
+
+			Console.WriteLine("Enter your Date of Birth (DD/MM/YYYY)");
+			//reads users input from console
+			string getDate = Console.ReadLine();
+
+			//gets the current date
+			int currentYear = DateTime.Now.Year;
+			int currentMonth = DateTime.Now.Month;
+			int currentDay = DateTime.Now.Day;
+
+			try
+			{
+				//converts user input to Date object
+				DateTime dob = Convert.ToDateTime(getDate);
+				//getting the dates
+				int birthYear = dob.Year;
+				int birthMonth = dob.Month;
+				int birthDay = dob.Day;
+				age = currentYear - birthYear;
+				if (currentMonth < birthMonth)
+				{
+					--age;
+				}
+				else if (currentMonth == birthMonth)
+				{
+					if (currentDay > birthDay)
+					{
+						--age;
+					}
+
+				}
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine("Incorrect date format");
+			}
+		
+
+
+			return age;
+		}
+		
+
+	}
+}
